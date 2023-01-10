0877faefd43e263c036a5e8ce55420b790ece945
JulianaRkg544 <julianna.rkg544@gmail.com>
2023-01-10 14:40:11 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int tabuada = 5; 
+        int x;
+        for (x = 0; x<=10; x++){
+            System.out.print("Tabuada do " + tabuada);
+            System.out.print(" "+ x + " x " + tabuada + " = ");
+            System.out.println(x * tabuada);
+        }
+        
+    }
+}
